acb5d230ade0c0295d1f4870a9ef527e5b468fbf
WilliamLoyd <info@twconsulting.biz>
2025-12-23 10:32:46 +0000

    deat: add clock logic and timing

--- main.py
+++ main.py
@@ -8,14 +8,17 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
     while True:
         log_state()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill
         pygame.display.flip()
-
+        dt = (clock.tick(60)/100)
         
 if __name__ == "__main__":
     main()
