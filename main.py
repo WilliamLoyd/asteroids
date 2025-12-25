fcdfbdb30d4842852d4bf8ec4c60eeaaa67ea29e
WilliamLoyd <info@twconsulting.biz>
2025-12-25 21:44:50 +0000

    feat: add .move() method and movement functionality

--- main.py
+++ main.py
@@ -20,10 +20,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill
+        player.update(dt)
+        screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 100
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
