a6ae056a7a20c669f0404c0a8900dc0a8b2afa8b
WilliamLoyd <info@twconsulting.biz>
2025-12-25 00:05:00 +0000

    feat: add player class and on -sceen functionality

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
+
 from constants import *
 from logger import log_state
+from player import Player
+
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -10,15 +13,18 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt = 0
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
-        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill
+        player.draw(screen)
         pygame.display.flip()
-        dt = (clock.tick(60)/100)
-        
+        dt = clock.tick(60) / 100
+
+
 if __name__ == "__main__":
     main()
