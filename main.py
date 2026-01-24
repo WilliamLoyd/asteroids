603d18ea392e3891463c99f750ef8dcc639bc682
William Loyd <info@twconsulting.biz>
2026-01-24 07:11:08 +0000

    Add two classes to handle asteroid spawning and movement

--- main.py
+++ main.py
@@ -3,19 +3,29 @@ import pygame
 from constants import *
 from logger import log_state
 from player import Player
+from asteroids import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    #initialize the pygame engine and set the screen
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    #init the delta time to 0 and creates an object that helps control the game's FPS
     dt = 0
     clock = pygame.time.Clock()
+    #creates "buckets" for objects that are updateable or drawable or both
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    #every new Player object created gets added to the "buckets" and then initialized to the middle of the screen
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -26,8 +36,9 @@ def main():
                 return
         updatable.update(dt)
         screen.fill("black")
+        #iterates over every item in the "drawable" bucket
         for item in drawable:
-            player.draw(screen)
+            item.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
